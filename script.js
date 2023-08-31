3c176eebb70d7f80f1e3078e3a99d501547ef261
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-08-31 23:55:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function firstWord(s) {
   // your code here
 	let a = s.split(" ");
-	if(a[0] === " "){
+	if(a[0] === ""){
 		return a[1];
 	}
 		return a[0];
 }
 // Do not change the code below
 
-// const s = prompt("Enter String:");
-// alert(firstWord(s));
+const s = prompt("Enter String:");
+alert(firstWord(s));
+
